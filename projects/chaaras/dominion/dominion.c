b0076320069316beeb16cee5c8a56671b1fc2756
SamChaara <38476989+SamChaara@users.noreply.github.com>
2019-04-21 14:45:56 -0500

    refactor steward to stewardEffect()

--- projects/chaaras/dominion/dominion.c
+++ projects/chaaras/dominion/dominion.c
@@ -984,7 +984,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
   case great_hall:
     return greatHallEffect(state, handPos);
-    
+
   case minion:
     //+1 action
     state->numActions++;
@@ -1036,27 +1036,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     return 0;
 
   case steward:
-    if (choice1 == 1)
-    {
-      //+2 cards
-      drawCard(currentPlayer, state);
-      drawCard(currentPlayer, state);
-    }
-    else if (choice1 == 2)
-    {
-      //+2 coins
-      state->coins = state->coins + 2;
-    }
-    else
-    {
-      //trash 2 cards in hand
-      discardCard(choice2, currentPlayer, state, 1);
-      discardCard(choice3, currentPlayer, state, 1);
-    }
-
-    //discard card from hand
-    discardCard(handPos, currentPlayer, state, 0);
-    return 0;
+    return stewardEffect(state, handPos, choice1, choice2, choice3);
 
   case tribute:
     if ((state->discardCount[nextPlayer] + state->deckCount[nextPlayer]) <= 1)
@@ -1492,6 +1472,35 @@ int greatHallEffect(struct gameState *state, int handPos)
 
   //discard card from hand
   discardCard(handPos, currentPlayer, state, 0);
+
+  return 0;
+}
+
+int stewardEffect(struct gameState *state, int handPos, int choice1, int choice2, int choice3)
+{
+  int currentPlayer = whoseTurn(state);
+
+  if (choice1 == 1)
+  {
+    //+2 cards
+    drawCard(currentPlayer, state);
+    drawCard(currentPlayer, state);
+  }
+  else if (choice1 == 2)
+  {
+    //+2 coins
+    state->coins = state->coins + 2;
+  }
+  else
+  {
+    //trash 2 cards in hand
+    discardCard(choice2, currentPlayer, state, 1);
+    discardCard(choice3, currentPlayer, state, 1);
+  }
+
+  //discard card from hand
+  discardCard(handPos, currentPlayer, state, 0);
+
   return 0;
 }
 
